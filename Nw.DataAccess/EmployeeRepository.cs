7758e5743053e46c67ae75bb0ed65b1bd94579fd
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2019-09-11 13:14:12 +0200

    Repository with method to get all employees is now created

---
+++ Nw.DataAccess/EmployeeRepository.cs
@@ -0,0 +1,20 @@
+﻿using Nw.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Nw.DataAccess
+{
+    public class EmployeeRepository
+    {
+        private NorthwindModel model;
+        public EmployeeRepository()
+        {
+            model = new NorthwindModel();
+        }
+        public List<Employees> GetAllEmployees()
+        {
+            return model.Employees.ToList();
+        }
+    }
+}
