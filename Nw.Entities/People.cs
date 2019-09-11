f4cfeda3723c4e2cfcec131a9b7a49ff578f57a2
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2019-09-11 09:29:15 +0200

    Init commit

---
+++ Nw.Entities/People.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Nw.Entities
+{
+    public partial class People
+    {
+        public int PersonId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
