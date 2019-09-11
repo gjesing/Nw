e685080590b2105760dabdd50ee5373545283885
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2019-09-11 11:44:33 +0200

    Index page created

---
+++ Nw.Web/Pages/HR.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Nw.Web.Pages
+{
+    public class HRModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
