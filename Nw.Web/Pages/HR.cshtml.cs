08e41c17120bba41bd75917a64eadc4ddf774832
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2019-09-12 14:28:49 +0200

    HR page layout and table with employees from database is now created

--- Nw.Web/Pages/HR.cshtml.cs
+++ Nw.Web/Pages/HR.cshtml.cs
@@ -4,14 +4,18 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Nw.DataAccess;
+using Nw.Entities;
 
 namespace Nw.Web.Pages
 {
     public class HRModel : PageModel
     {
+        public List<Employees> Employees { get; set; } = new List<Employees>();
         public void OnGet()
         {
-
+            EmployeeRepository employeeRepository = new EmployeeRepository();
+            Employees = employeeRepository.GetAllEmployees();
         }
     }
 }
