b2c946bfac004abbf6727e33bf8f267efb55d81a
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2019-09-16 11:58:44 +0200

    OrderRepository created with method to get all orders from the database

---
+++ Nw.DataAccess/OrderRepository.cs
@@ -0,0 +1,21 @@
+﻿using Nw.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nw.DataAccess
+{
+    class OrderRepository
+    {
+        private NorthwindModel model;
+        public OrderRepository()
+        {
+            model = new NorthwindModel();
+        }
+        public List<Orders> GetAllOrders()
+        {
+            return model.Orders.ToList();
+        }
+    }
+}
