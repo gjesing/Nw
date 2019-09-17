d4247419023e2d60b355f2f742fbb17b110d0216
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2019-09-17 11:14:34 +0200

    OrderDetails page is now created in Nw.Web

---
+++ Nw.Web/Pages/OrderDetails.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Nw.Entities;
+
+namespace Nw.Web.Pages
+{
+    public class OrderDetailsModel : PageModel
+    {
+        public Orders Order { get; set; }
+        public List<OrderDetails> OrderDetails { get; set; } = new List<OrderDetails>();
+        public IActionResult OnGet(int id)
+        {
+            return Page();
+        }
+    }
+}
