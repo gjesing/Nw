dd97f774de29d89b4b38c8a10685a2a36938574e
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2019-09-17 10:40:17 +0200

    Method GetOrderDetailByOrderId() is now created in OrderDetailRepository

--- Nw.DataAccess/OrderDetailRepository.cs
+++ Nw.DataAccess/OrderDetailRepository.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿using Nw.Entities;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Nw.DataAccess
 {
     public class OrderDetailRepository
     {
+        private NorthwindModel model;
+        public OrderDetails GetOrderDetailByOrderId(int id)
+        {
+            return model.OrderDetails.Where(o => o.OrderId == id).First();
+        }
     }
 }
